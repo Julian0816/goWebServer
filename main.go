a8ce2dad7d232c39aeabe39736fdc174e4209ad7
Julian0816 <83423059+Julian0816@users.noreply.github.com>
2023-12-10 12:13:49 +0000

    Set up postgress

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
-	"os"
 	"net/http"
-	
+	"os"
 
-	"github.com/joho/godotenv"
+	"github.com/Julian0816/rssagg/internal/database"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 
@@ -19,6 +26,20 @@ func main() {
 	portString := os.Getenv("PORT")
 	if (portString == "") {
 		log.Fatal("PORT is not found in the environment") // Exit the program immediately with error code 1 and a message
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if (dbURL == "") {
+		log.Fatal("DB_URL is not found in the environment") // Exit the program immediately with error code 1 and a message
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to database:", err) //
+	}
+	
+	apiCfg := apiConfig{
+		DB: database.New(conn),
 	}
 
 	router := chi.NewRouter()
@@ -36,6 +57,7 @@ func main() {
    v1Router := chi.NewRouter()
    v1Router.Get("/healthz", handlerReadiness) // Connect the handlerReadiness to the "/healthz" path
    v1Router.Get("/err", handlerErr)
+   v1Router.Post("/users", apiCfg.handlerCreateUser) // Create User handler
 
    router.Mount("/v1", v1Router) // nest a v1Router under the v1 path // This is good practive in case you need to make a v2 route
 
@@ -47,7 +69,7 @@ func main() {
 	}
 	
 	log.Printf("Server starting on port %v", portString)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
